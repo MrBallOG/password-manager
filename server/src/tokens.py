30d61db4724eb0e50182a85d783cdf0e8df2df9a
Hubert Piłka <hubert.pilka@outlook.com>
2022-01-31 00:13:52 +0100

    token expiration date fix

--- server/src/tokens.py
+++ server/src/tokens.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from fastapi import HTTPException
 import jwt
 from pydantic import BaseModel
+from sqlalchemy import true
 from settings import settings
 
 
@@ -11,38 +12,41 @@ class Token(BaseModel):
 
 
 def create_refresh_token(email: str) -> Token:
-    return Token(token=create_token(email, None), token_type="refresh_token")
+    return Token(token=create_token(email, True), token_type="refresh_token")
 
 
 def create_access_token(email: str) -> Token:
     return Token(token=create_token(email), token_type="bearer")
 
 
-def create_token(email: str, duration: int = settings.TOKEN_DURATION) -> str:
-    if duration == None:
+def create_token(email: str, refresh: bool = False) -> str:
+    if refresh:
+        duration = 64 * settings.TOKEN_DURATION
         secret = settings.SECRET_REFRESH
-        to_encode = {"sub": email}
+        expire = datetime.utcnow() + timedelta(minutes=duration)
     else:
+        duration = settings.TOKEN_DURATION
         secret = settings.SECRET_ACCESS
         expire = datetime.utcnow() + timedelta(minutes=duration)
-        to_encode = {"sub": email, "exp": expire}
+
+    to_encode = {"sub": email, "exp": expire}
 
     return jwt.encode(to_encode, secret, algorithm=settings.ALGORITHM)
 
 
 def verify_refresh_token(token: Token) -> str:
-    return verify_token(token, "REFRESH")
+    return verify_token(token, True)
 
 
 def verify_access_token(token: Token) -> str:
     return verify_token(token)
 
 
-def verify_token(token: Token, type: str = "ACCESS") -> str:
-    if type == "ACCESS":
-        secret = settings.SECRET_ACCESS
-    else:
+def verify_token(token: Token, refresh: bool = False) -> str:
+    if refresh:
         secret = settings.SECRET_REFRESH
+    else:
+        secret = settings.SECRET_ACCESS
 
     try:
         data = jwt.decode(token.token, secret, algorithms=settings.ALGORITHM)
